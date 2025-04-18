2257c066e3baf2b5d99882d15aa2d1278227e4c4
¨kevin180317¨ <ortega1gv@gmail.com>
2025-04-18 10:09:31 -0700

    upadte footer and drag

--- src/components/Success_Stories.jsx
+++ src/components/Success_Stories.jsx
@@ -7,6 +7,8 @@ const SuccessStories = ({ URL }) => {
   const [animationKey, setAnimationKey] = useState(0);
   const [isActive, setIsActive] = useState(false); // Estado para controlar la activación de la animación
   const sectionRef = useRef(null); // Ref para la sección a observar
+  const [dragStatus, setDragStatus] = useState(""); // Estado para el estado del drag
+  const [cursorStyle, setCursorStyle] = useState("default"); // Estado para el estilo del cursor
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -159,6 +161,22 @@ const SuccessStories = ({ URL }) => {
     setAnimationKey((prevKey) => prevKey + 1);
   }
 
+  const handleDragStart = () => {
+    setDragStatus("dragging");
+    setCursorStyle("grabbing"); // Cambia el cursor a "grabbing" al iniciar el drag
+  };
+
+  const handleDragEnd = (event, info) => {
+    setDragStatus("dropped");
+    setCursorStyle("default"); // Restablece el cursor al soltar
+    if (info.offset.x > 100) {
+      decrementIndex(); // Swipe right
+    } else if (info.offset.x < -100) {
+      incrementIndex(); // Swipe left
+    }
+    setTimeout(() => setDragStatus(""), 500); // Reset drag status
+  };
+
   useEffect(() => {
     if (animate) {
       const timer = setTimeout(() => setAnimate(false), 1000);
@@ -167,8 +185,11 @@ const SuccessStories = ({ URL }) => {
   }, [animate, currentIndex]);
 
   return (
-    <main className="flex flex-col lg:h-screen py-16 lg:py-0">
-      <div ref={sectionRef} className="overflow-hidden h-screen">
+    <main
+      className="flex flex-col lg:h-screen py-16 lg:py-0"
+      style={{ cursor: cursorStyle }} // Aplica el estilo del cursor dinámicamente
+    >
+      <div ref={sectionRef} className="overflow-hidden h-screen relative">
         <motion.div
           key={currentIndex}
           initial={{ scale: 1.25 }}
@@ -183,102 +204,120 @@ const SuccessStories = ({ URL }) => {
                 : content_es[currentIndex].background
             })`,
           }}
+          className="absolute inset-0"
+        />
+        <motion.div
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          onMouseEnter={() => setCursorStyle("grab")} // Cambia el cursor a "grab" al pasar el mouse
+          onMouseLeave={() => setCursorStyle("default")} // Restablece el cursor al salir
+          className="relative z-10 flex justify-center lg:justify-between items-center h-screen lg:w-[1400px] m-auto px-28"
         >
-          <div className="flex justify-center lg:justify-between items-center h-screen lg:w-[1400px] m-auto px-28">
-            <button onClick={decrementIndex} className="hidden lg:block">
-              <svg
-                width="31"
-                height="57"
-                viewBox="0 0 31 57"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-label="Previous Slide"
-              >
-                <path
-                  d="M28.2861 54.6339L2.41885 28.1918L28.861 2.32457"
-                  stroke="#969596"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-            <motion.div
-              key={animationKey}
-              className={`flex flex-col justify-center items-start px-16`}
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.0 }}
+          <button onClick={decrementIndex} className="hidden lg:block">
+            <svg
+              width="31"
+              height="57"
+              viewBox="0 0 31 57"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-label="Previous Slide"
             >
-              <h2 className="font-bold text-[25px] lg:text-[50px] mb-2">
-                {URL
-                  ? content_en[currentIndex].title
-                  : content_es[currentIndex].title}
-              </h2>
-              <h2 className="font-bold text-[70px] lg:text-[80px] mb-2">
-                {URL
-                  ? content_en[currentIndex].title2
-                  : content_es[currentIndex].title2}
-              </h2>
-              <h3 className="font-bold text-[14px] lg:text-[18px] text-[#969596] mb-8 lg:w-[70%]">
-                {URL
-                  ? content_en[currentIndex].subtitle
-                  : content_es[currentIndex].subtitle}
-              </h3>
-              <button className="bg-blue-500 hover:bg-blue-700 px-8 py-2 mb-12">
-                <a
-                  className="text-sm"
-                  href={
-                    URL
-                      ? content_en[currentIndex].link
-                      : content_es[currentIndex].link
-                  }
-                >
-                  {URL ? "Learn more" : "Conoce más"}
-                </a>
-              </button>
-              <div className="flex justify-center items-center gap-4">
-                {(URL ? content_en : content_es).map((item, index) => (
-                  <svg
-                    className="cursor-pointer"
-                    key={index}
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    onClick={() => setCurrentIndex(index)}
-                  >
-                    <circle
-                      cx="6"
-                      cy="6"
-                      r="6"
-                      fill={currentIndex === index ? "#3B82F6" : "#FFFFFF"}
-                    />
-                  </svg>
-                ))}
-              </div>
-            </motion.div>
-            <button onClick={incrementIndex} className="hidden lg:block">
-              <svg
-                width="31"
-                height="57"
-                viewBox="0 0 31 57"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-label="Next Slide"
+              <path
+                d="M28.2861 54.6339L2.41885 28.1918L28.861 2.32457"
+                stroke="#969596"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+          <motion.div
+            key={animationKey}
+            className={`flex flex-col justify-center items-start px-16 ${
+              dragStatus === "dragging" ? "opacity-50" : "opacity-100"
+            }`}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.0 }}
+          >
+            <h2 className="font-bold text-[25px] lg:text-[50px] mb-2">
+              {URL
+                ? content_en[currentIndex].title
+                : content_es[currentIndex].title}
+            </h2>
+            <h2 className="font-bold text-[70px] lg:text-[80px] mb-2">
+              {URL
+                ? content_en[currentIndex].title2
+                : content_es[currentIndex].title2}
+            </h2>
+            <h3 className="font-bold text-[14px] lg:text-[18px] text-[#969596] mb-8 lg:w-[70%]">
+              {URL
+                ? content_en[currentIndex].subtitle
+                : content_es[currentIndex].subtitle}
+            </h3>
+            <button className="bg-blue-500 hover:bg-blue-700 px-8 py-2 mb-12">
+              <a
+                className="text-sm"
+                href={
+                  URL
+                    ? content_en[currentIndex].link
+                    : content_es[currentIndex].link
+                }
               >
-                <path
-                  d="M2.42188 2L28.5781 28.1562L2.42188 54.3125"
-                  stroke="#969596"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+                {URL ? "Learn more" : "Conoce más"}
+              </a>
             </button>
-          </div>
+            <div className="flex justify-center items-center gap-4">
+              {(URL ? content_en : content_es).map((item, index) => (
+                <svg
+                  className="cursor-pointer"
+                  key={index}
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={() => setCurrentIndex(index)}
+                >
+                  <circle
+                    cx="6"
+                    cy="6"
+                    r="6"
+                    fill={currentIndex === index ? "#3B82F6" : "#FFFFFF"}
+                  />
+                </svg>
+              ))}
+            </div>
+          </motion.div>
+          <button onClick={incrementIndex} className="hidden lg:block">
+            <svg
+              width="31"
+              height="57"
+              viewBox="0 0 31 57"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-label="Next Slide"
+            >
+              <path
+                d="M2.42188 2L28.5781 28.1562L2.42188 54.3125"
+                stroke="#969596"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
         </motion.div>
+        {dragStatus === "dragging" && (
+          <motion.div
+            className="absolute inset-0 bg-black bg-opacity-25"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          />
+        )}
       </div>
     </main>
   );
